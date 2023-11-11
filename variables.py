83d2d487ff053c65a69d46d2aff8b461dde7d12c
Edgar <150549434+Bronwyn214@users.noreply.github.com>
2023-11-11 15:46:21 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,42 @@
+print(22)
+print(1.5)
+print('Hola mundo')
+print(True)
+print(False)
+
+#######################################################
+
+nombre= 'soy edgar'
+edad= 20
+
+presentate=(f'Me presento, {nombre}')
+print(presentate)
+
+ed_pre= (f'Tengo {edad}')
+print(ed_pre)
+
+base,altura,color= 5,7,"verde"
+
+print(base)
+print(altura)
+print(color)
+
+################################################################
+
+lado_1= 3
+lado_2= 5
+lado_3= 12
+
+perimetro= lado_1 + lado_2 + lado_3
+print(f'el perimetro es {perimetro}')
+
+################################################################
+# FORMULA GENERAL
+
+valor_A= 7
+valor_b= -13
+valor_c= 6
+
+formula= (-valor_b + ((valor_b**2)-(4*valor_A*valor_c))**(1/2))/(2*valor_A)
+
+print(f'mi resultado es {int(formula)}')
