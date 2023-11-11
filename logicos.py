f979a8bb31cfe4286c4e20fc58cf4658caf9f3a7
Edgar <150549434+Bronwyn214@users.noreply.github.com>
2023-11-11 16:03:59 -0500

    Create logicos.py

---
+++ logicos.py
@@ -0,0 +1,52 @@
+#CREE UN PROGRAMA QUE IMPRIMA TRUE SI EL ENTERO GUARDADO EN LA VARIABLE ES PAR, SINO IMPRIMA FALSE
+
+VA= 58
+enva= VA % 2
+
+print(enva) #0
+
+cond= enva ==0
+
+print( cond) #TRUE
+
+Vb= 77
+envb= Vb % 2
+
+print(envb) #1
+
+condd= envb ==0
+
+print( condd) #FALSE
+
+#################################################
+
+2>1 and 4<8 #T Y T = T
+
+print(2>1 and 4<8) #true
+
+9!=6 or 7<=3 #T o F = T
+
+print(9!=6 or 7<=3) #true
+
+#IMPRIMIR SI LA EDAD ES PAR
+
+edk= 22
+ej= (edk%2) == 0 #TRUE
+
+#SI EL MODULO SALE 0 ES PORQUE ES PAR, SI PAR ES IGUAL A PAR ES TRUE
+print(ej) #TRUE
+
+#Y ADEMAS QUE NO SEA MAYOR A 24
+BR= not(edk>24)
+
+#not(false)= true
+
+print(BR) #TRUE
+
+CONDC= ej and BR #QUE SEA PAR Y QUE NO SEA MAYOR A 24
+#T Y T = T
+
+print(CONDC)
+
+
+#############################################################
