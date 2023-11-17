332602b48c7fc115c4aff79c052eb5db2edd2858
Edgar <150549434+Bronwyn214@users.noreply.github.com>
2023-11-16 19:23:24 -0500

    Update listas-random.py

--- listas-random.py
+++ listas-random.py
@@ -54,6 +54,60 @@ print(unir)
 
 user(input('ingrese su palabra: '))
 
+#####################################################################################################################
+pokemones= ['Pikachu', 'Braixen', 'Primarina', 'Lucario'] #nombre de los pokemon
+niveles= [15, 50, 20, 30 ] #poderes de los pokemon
+
+poke= input('Ingrese el nombre de un pokemon: ') #'Braixen'
+print(poke)
+
+
+escojo= choice(pokemones) #'Lucario'
+print(f'Pokemon rival {escojo}')
+
+poder1= pokemones.index(poke)
+poderdef= niveles[poder1]
+print(f'Poder del primer poke {poderdef}')
+
+poder2= pokemones.index(escojo)
+poderdef2= niveles[poder2]
+print(f'Poder del segundo poke {poderdef2}')
+
+cond= poderdef > poderdef2
+cond2= poderdef2 > poderdef
+
+condfin= cond or cond2
+
+print(f'Ganaste {condfin}')
+
 cond= user == escojo
 
 print(f'gano {cond}')
+
+#####################################################################################################################################
+
+personas= ['Juan Perez', 'Carlos Leon', 'Francisco Leon', 'CARLOS MALDONADO']
+
+escojo= sample(personas, 2)
+print(escojo) #['CARLOS MALDONADO', 'Juan Perez']
+
+primer= escojo[0].title() #'Carlos Maldonado'
+segundo= escojo[-1].title() #'Juan Perez'
+
+pos= primer.index(' ')
+ind= primer[pos+1 :] #'Maldonado'
+#print(ind)
+
+
+pos2= segundo.index(' ')
+ind2= segundo[pos2+1 :] #'Perez'
+#print(ind2)
+
+print(f'Persona 1: {primer}')
+print(f'Persona 2: {segundo}')
+
+
+if ind == ind2:
+    print('Son parientes')
+else:
+    print('No son parientes')
